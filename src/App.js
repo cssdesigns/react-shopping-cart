ee1c280fe08916dbb6a8b77716475a614382df85
cssdesigns <satishkmr485@gmail.com>
2020-09-25 12:29:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>all rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
